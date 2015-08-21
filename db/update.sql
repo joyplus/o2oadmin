7ff00b06f63b0a345a53d8bc4e53340d660395b7
Scott Li <scottliyq@gmail.com>
2015-08-21 22:47:17 +0800

    fix a typo

--- db/update.sql
+++ db/update.sql
@@ -3,5 +3,5 @@ INSERT INTO `node` (`id`,`title`,`name`,`level`,`pid`,`remark`,`status`,`group_i
 INSERT INTO `node` (`id`,`title`,`name`,`level`,`pid`,`remark`,`status`,`group_id`) VALUES (32,'PDB广告位管理','adspace/index',2,31,NULL,2,2);
 INSERT INTO `node` (`id`,`title`,`name`,`level`,`pid`,`remark`,`status`,`group_id`) VALUES (33,'DSP管理','demand/index',2,31,NULL,2,2);
 INSERT INTO `node` (`id`,`title`,`name`,`level`,`pid`,`remark`,`status`,`group_id`) VALUES (34,'PDB媒体管理','media/index',2,31,NULL,2,2);
-INSERT INTO `node` (`id`,`title`,`name`,`level`,`pid`,`remark`,`status`,`group_id`) VALUES (35,'PDB媒体报表','Report/GetPdbMediaReport',2,31,NULL,2,2);
+INSERT INTO `node` (`id`,`title`,`name`,`level`,`pid`,`remark`,`status`,`group_id`) VALUES (35,'PDB媒体报表','report/GetPdbMediaReport',2,31,NULL,2,2);
 INSERT INTO `node` (`id`,`title`,`name`,`level`,`pid`,`remark`,`status`,`group_id`) VALUES (36,'DSP报表','report/GetPdbDspReport',2,31,NULL,2,2);
