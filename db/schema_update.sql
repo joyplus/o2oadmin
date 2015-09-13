d0451e168b29bf38646d068a4bd24f036a6733d2
Scott Li <scottliyq@gmail.com>
2015-09-13 23:29:01 +0800

    change db script

--- db/schema_update.sql
+++ db/schema_update.sql
@@ -57,3 +57,103 @@ UPDATE `pmp_media` SET `del_flg` = 0;
 
 ALTER TABLE `pmp_demand_adspace` ADD `real_adspace_key` VARCHAR(50) NOT NULL AFTER `update_time`;
 ALTER table pmp_demand_adspace add UNIQUE UNIQUE_DEMAND_ADSPACE_KEY(demand_adspace_key);
+
+
+/*20150913*/
+-- -----------------------------------------------------
+-- Table `pmp`.`pmp_campaign`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pmp`.`pmp_campaign` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `group_id` INT NOT NULL,
+  `name` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL,
+  `start_date` DATE NULL DEFAULT NULL,
+  `end_date` DATE NULL DEFAULT NULL,
+  `campaign_status` INT(11) NOT NULL COMMENT '0：暂停1： 运行',
+  `landing_url` VARCHAR(500) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL,
+  `demand_adspace_id` INT(11) NOT NULL,
+  `imp_tracking_url` VARCHAR(1000) NULL,
+  `clk_tracking_url` VARCHAR(1000) NULL,
+  `landing_url` VARCHAR(1000) NULL,
+  `ad_type` INT NULL,
+  `campaign_type` INT NULL COMMENT '0: 开屏，1: banner, 2: 信息流',
+  `accurate_type` INT NULL COMMENT '0:精准,1:适中,2:广泛',
+  `pricing_type` INT NULL COMMENT '0:cpc,1:cpm,2:RD',
+  `strategy_type` INT NULL,
+  `budget_type` INT NULL,
+  `budget` INT NULL,
+  `bid_price` FLOAT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
+/*roll back some records*/
+INSERT INTO `pmp_campaign` (`id`, `name`, `start_date`, `end_date`, `campaign_status`, `landing_url`, `demand_adspace_id`) VALUES
+(1, '开屏', '2015-06-30', '2015-09-30', 1, 'http://www.baidu.com', 9),
+(2, 'banner_normal', '2015-06-30', '2015-09-30', 1, 'http://www.baidu.com', 10),
+(3, 'banner_webview', '2015-06-30', '2015-09-30', 1, 'http://www.baidu.com', 11);
+
+
+-- -----------------------------------------------------
+-- Table `pmp`.`pmp_campaign_group`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pmp`.`pmp_campaign_group` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `advertiser_Id` INT NOT NULL,
+  `name` VARCHAR(50) NOT NULL,
+  `budget` INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pmp`.`pmp_advertiser`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pmp`.`pmp_advertiser` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `description` VARCHAR(500) NULL,
+  `advertiser_type` INT NOT NULL DEFAULT 0 COMMENT '0: 广告主；1：代理商',
+  `del_flg` INT NOT NULL DEFAULT 0,
+  `category_key` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pmp`.`pmp_lov`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pmp`.`pmp_lov` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `lov_code` VARCHAR(45) NOT NULL,
+  `lov_key` INT NOT NULL,
+  `lov_value` VARCHAR(45) NOT NULL,
+  `disp_order` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pmp`.`pmp_campaign_daily_config`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pmp`.`pmp_campaign_daily_config` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `campaign_id` INT NOT NULL,
+  `week_day` INT NOT NULL,
+  `target_hours` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `pmp`.`pmp_campaign_targeting`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pmp`.`pmp_campaign_targeting` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `campaign_id` INT NOT NULL,
+  `targeting_type` VARCHAR(45) NULL,
+  `targeting_id` INT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
